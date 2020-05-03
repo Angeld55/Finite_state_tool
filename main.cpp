64e1d9fa8481ed49cb767474fa9b046e1b18fdfb
Angeld55 <angel_dimitriev@abv.bg>
2020-05-03 21:30:17 +0300

    fixed bugs

--- main.cpp
+++ main.cpp
@@ -2,14 +2,21 @@
 
 int main() 
 {	
+	//Example for NFA with reg. expr: (a(a+b)*b) + (b(a+b)*a)
+
 
 	FiniteStateAutomation a("((((a).(((a)+(b)))*).(b))+(((b).(((a)+(b)))*).(a)))"); // (a(a+b)*b) + (b(a+b)*a)
+
+	std::cout << a.Accept("abba") << std::endl; //false
+	std::cout << a.Accept("abbb") << std::endl; //true
+	std::cout << a.Accept("aaabbbbaaaaab") << std::endl; //false
 
 	a.Minimize();
 	
 	a.Print();
 
 	std::cout<<a.GetRegEx()<<std::endl;
-   	return 0;
+   	
+	return 0;
 }
  
