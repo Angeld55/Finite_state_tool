7a59288d953f39f1866593ad1f8d365caec0a958
Angel Dimitriev <achi_d@abv.bg>
2020-11-17 02:18:47 +0200

    Update NPDA.cpp

--- Non-deterministic_pushdown_automaton/NPDA.cpp
+++ Non-deterministic_pushdown_automaton/NPDA.cpp
@@ -46,35 +46,31 @@ void NPDA::AddTransition(int initialState, char symbol, char stackTopSymbol, int
 {
 	rules.push_back({ initialState, symbol, stackTopSymbol, destState, stringToReplaceTopStackSymbol });
 }
-void NPDA::ApplyRuleIfPossible(Computation& current, Rule& ruleToApply, queue<Computation>& q)
+void PDA::ApplyRuleIfPossible(Computation& current, Rule& ruleToApply, queue<Computation>& q)
 {
-	if (current.state == ruleToApply.initialState)
+	if ((current.state == ruleToApply.initialState) 
+		&&((ruleToApply.symbol == '$') || (current.word[0] == ruleToApply.symbol))
+		&& (ruleToApply.stackTopSymbol == '$' || current.st.top() == ruleToApply.stackTopSymbol))
 	{
-		if ((ruleToApply.symbol == '$') || (current.word[0] == ruleToApply.symbol))
-		{
-			if (ruleToApply.stackTopSymbol == '$' || current.st.top() == ruleToApply.stackTopSymbol)
-			{
-				Computation newComputation;
 
-				newComputation.computationSteps = current.computationSteps + 1;
+		Computation newComputation;
+
+		newComputation.computationSteps = current.computationSteps + 1;
 				
-				newComputation.state = ruleToApply.destState;
-				newComputation.word = ruleToApply.symbol == '$' ? current.word : current.word.substr(1);
-				newComputation.st = current.st;
+		newComputation.state = ruleToApply.destState;
+		newComputation.word = ruleToApply.symbol == '$' ? current.word : current.word.substr(1);
+		newComputation.st = current.st;
 
-				if (ruleToApply.stackTopSymbol != '$')
-					newComputation.st.pop();
+		if (ruleToApply.stackTopSymbol != '$')
+			newComputation.st.pop();
 
-				if (ruleToApply.stringToReplaceTopStackSymbol != "$")
-				{
-					for (int i = ruleToApply.stringToReplaceTopStackSymbol.size() - 1; i >= 0; i--)
-						newComputation.st.push(ruleToApply.stringToReplaceTopStackSymbol[i]);
-				}
-								
-				q.push(newComputation);
-			}
+		if (ruleToApply.stringToReplaceTopStackSymbol != "$")
+		{
+			for (int i = ruleToApply.stringToReplaceTopStackSymbol.size() - 1; i >= 0; i--)
+				newComputation.st.push(ruleToApply.stringToReplaceTopStackSymbol[i]);
 		}
-		
+								
+		q.push(newComputation);
 	}
 }
 void NPDA::printComputation(const Computation& c)
