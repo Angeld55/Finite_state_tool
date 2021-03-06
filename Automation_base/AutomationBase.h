27b01d34c44678f6b1d753c1dba575d7620ac46f
Angel Dimitriev <achi_d@abv.bg>
2021-03-06 16:40:23 +0200

    Add files via upload

--- Automation_base/AutomationBase.h
+++ Automation_base/AutomationBase.h
@@ -15,7 +15,7 @@ public:
 	virtual bool accepts(const std::string& word, std::string& computation, bool shouldReturnComputation = false) const = 0;
 	virtual int addState() = 0;
 	virtual int addTransition(const std::vector<std::string>& args) = 0;
-	virtual bool makeStateFinal(size_t ind) = 0;
+	virtual int makeStateFinal(size_t ind) = 0;
 
 	virtual std::string getString() const = 0;
 	virtual std::string getVisualizeString() const = 0;
